35b68b4bf9a06e3f9c7bfbf9d3706e5b4ff9c701
Gabriel Lucero <lucero.gabriel.tadeo@gmail.com>
2023-09-19 19:13:00 -0300

    adding gallery preview carousel and modals

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Mobile hamburger menu function
+
 function hamburgerMenu() {
     const menuItems = document.getElementById("menuLinks");
     menuItems.classList.toggle('show');
@@ -16,64 +18,3 @@ document.addEventListener("click", function (event) {
         menu.classList.remove('show');
     }
 });
-
-function fadeInElements() {
-    const elements = document.querySelectorAll('.fade-in');
-
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1 // Adjust this threshold to control when the fade-in effect triggers
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, observerOptions);
-
-    elements.forEach(element => {
-        observer.observe(element);
-    });
-}
-
-window.onload = function gallery() {
-
-    for (let i = 1; i < 91; i += 3) {
-        const gallery = document.getElementById('gallery');
-
-        var contenedorImags = document.createElement("div");
-        contenedorImags.className = "gallery-module";
-
-        var imgGrande = document.createElement("img");
-        imgGrande.setAttribute("src", `./images/Fotos/${i}.jpg`);
-        imgGrande.className = "big-image";
-
-        var contenedorImagChicas = document.createElement("div");
-        contenedorImagChicas.className = "small-images"
-
-        var imgChica1 = document.createElement("img");
-        imgChica1.setAttribute("src", `./images/Fotos/${i + 1}.jpg`);
-
-        var imgChica2 = document.createElement("img");
-        imgChica2.setAttribute("src", `./images/Fotos/${i + 2}.jpg`);
-
-        if (gallery) {
-            gallery.appendChild(contenedorImags);
-            if (i % 2) {
-                contenedorImags.appendChild(imgGrande);
-                contenedorImags.appendChild(contenedorImagChicas);
-            } else {
-                contenedorImags.appendChild(contenedorImagChicas);
-                contenedorImags.appendChild(imgGrande);
-            }
-            contenedorImagChicas.appendChild(imgChica1);
-            contenedorImagChicas.appendChild(imgChica2);
-        }
-    }
-}
-
-
